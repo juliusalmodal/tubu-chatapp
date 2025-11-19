6e783180d7c7caddbf0f899a9a8a6bc6ff8f7aa8
Julius Almodal <julius.almodal@outlook.com>
2025-11-20 01:10:40 +0800

    Fixed API_URL

--- src/ChatPage.jsx
+++ src/ChatPage.jsx
@@ -2,7 +2,8 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import ChatBubble from "./ChatBubble";
 
-const API_URL = "http://localhost:7071/api/messages";
+//const API_URL = "http://localhost:7071/api/messages";
+const API_URL = "https://tubu-chat-app-hya8b9nhguh9gucs.southeastasia-01.azurewebsites.net/api/messages";
 
 export default function ChatPage() {
   const { userId } = useParams();
